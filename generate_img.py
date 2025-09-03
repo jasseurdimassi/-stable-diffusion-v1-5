3c1a1f1ad5f65ed828cce4216ec03788c489c7fc
jasseurdimassi <jasseur.dimassi@gmail.com>
2025-09-03 11:04:17 +0100

    Update generate_img.py

--- generate_img.py
+++ generate_img.py
@@ -5,18 +5,18 @@ from diffusers.utils import load_image, make_image_grid
 # Charger le pipeline en mode CPU uniquement
 pipeline = AutoPipelineForImage2Image.from_pretrained(
     "runwayml/stable-diffusion-v1-5",
-    torch_dtype=torch.float32,   # CPU → float32
+    dtype=torch.float32,   # CPU → float32
     use_safetensors=True
 )
 
 # Pas besoin de offload ni xformers sur CPU
 
 # Image de départ
-url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/img2img-init.png"
+url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/cat.png"
 init_image = load_image(url)
 
 # Prompt de génération
-prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
+prompt = "cat wizard, gandalf, lord of the rings, detailed, fantasy, cute, adorable, Pixar, Disney, 8k"
 
 # Génération (⚠️ ça peut prendre 3-10 minutes sur CPU selon la machine)
 image = pipeline(prompt=prompt, image=init_image, strength=0.8).images[0]
